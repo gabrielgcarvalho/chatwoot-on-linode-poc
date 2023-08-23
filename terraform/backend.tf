d57ab344029ab68545ab5998ffa62db442ec6dd7
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-23 02:47:12 -0300

    Add Backend and Provider definition

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    acl                  = "bucket-owner-full-control"
+    bucket               = "cloud-infrastructure-hub-terraform-state"
+    dynamodb_table       = "cloud-infrastructure-hub-terraform-state-lock"
+    encrypt              = true
+    key                  = "poc/chatwoot-on-linode/terraform.tfstate"
+    region               = "us-east-2"
+  }
+}
