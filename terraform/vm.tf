7a0333e72f73214444b621950fe147e24f437dc0
Gabriel G <gabrielgcarvalho.br@gmail.com>
2023-08-24 00:38:31 -0300

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,9 +1,13 @@
-resource "linode_instance" "terraform-web" {
+resource "linode_instance" "chatwoot-web" {
     image = "linode/ubuntu22.04"
-    label = "Chatwoot-full"
+    label = "Chatwoot"
     group = "chatwoot"
     region = "ca-central"
     type = "g6-standard-1" # 2 vCPU 
     authorized_keys = [ var.vm_public_key ]
     root_pass = var.vm_root_password
+}
+
+output "public_ip" {
+  value = linode_instance.chatwoot-web.ip_address
 }
